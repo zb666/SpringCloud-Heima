161bbaec3195ecb16ddeec0686ac1395bba96e9c
zb666 <1360799362@qq.com>
2020-10-08 15:28:34 +0800

    Eureka 集成 根据服务名称去获取实例对象

--- consumer-demo/src/main/java/com/itheima/ConsumerApplication.java
+++ consumer-demo/src/main/java/com/itheima/ConsumerApplication.java
@@ -2,11 +2,12 @@ package com.itheima;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringCloudApplication
+@EnableDiscoveryClient
 public class ConsumerApplication {
 
     public static void main(String[] args) {
--- consumer-demo/src/main/java/com/itheima/controller/ConsumerController.java
+++ consumer-demo/src/main/java/com/itheima/controller/ConsumerController.java
@@ -2,10 +2,15 @@ package com.itheima.controller;
 
 import com.itheima.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/consumer")
@@ -14,10 +19,20 @@ public class ConsumerController {
     @Autowired
     private RestTemplate restTemplate;
 
-    @GetMapping("/aaa")
-    public User queryById(Long id){
-        String url = "http://localhost:9091/user/8";
-        return restTemplate.getForObject(url,User.class);
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/{id}")
+    public User queryById(@PathVariable Long id){
+//        String url = "http://localhost:9091/user/8";
+//        return restTemplate.getForObject(url,User.class);
+        List<ServiceInstance> serviceInstanceList = discoveryClient.getInstances("user-service");
+        ServiceInstance instance = serviceInstanceList.get(0);
+        StringBuilder userServiceUrl = new StringBuilder().append("http://").append(instance.getHost()).append(":")
+                .append(instance.getPort())
+                .append("/user/")
+                .append(id);
+        return restTemplate.getForObject(userServiceUrl.toString(),User.class);
     }
 
 }
